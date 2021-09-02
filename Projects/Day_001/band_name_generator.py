baf9fb71cf51ae8852411e6eaf0995c2959a9c21
Evans Etrue Howard <sirmore.howard@gmail.com>
2021-09-03 00:38:17 +0200

    Day 1 completed
    Day 1 completed

--- Projects/Day_001/band_name_generator.py
+++ Projects/Day_001/band_name_generator.py
@@ -8,5 +8,5 @@ city = input("What is the name of the city you grew up in?\n")
 #3. Ask the user for the name of a pet.
 pet = input("What's the name of your pet?\n")
 
-#4. Combine the name of their city and pet and show them their band name.
+# 4. Combine the name of their city and pet and show them their band name.
 print("Your band name could be " + city + " " + pet)
