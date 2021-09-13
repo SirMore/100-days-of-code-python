c9397fd237daf91d81f57558cc89bb105022b661
Evans Etrue Howard <sirmore.howard@gmail.com>
2021-09-13 22:50:52 +0200

    Update main.py

--- Projects/Day_007/main.py
+++ Projects/Day_007/main.py
@@ -1,7 +1,20 @@
 import random
 from hangman_art import stages, logo
 from hangman_words import word_list
-from replit import clear
+#from replit import clear
+# import only system from os 
+from os import system, name 
+  
+# define our clear function 
+def clear(): 
+  
+    # for windows 
+    if name == 'nt': 
+        _ = system('cls') 
+  
+    # for mac and linux(here, os.name is 'posix') 
+    else: 
+        _ = system('clear') 
 
 print(logo)
 game_is_finished = False
